aa332e97dd7a36aa7f6a88e49810d942418e011a
Azzam Jihad Ulhaq <azzam.jihad@mekari.com>
2021-11-12 14:11:33 +0700

    refactor controller

---
+++ controllers/users/users_controller.go
@@ -0,0 +1,19 @@
+package users
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CreateUser(c *gin.Context) {
+	c.String(http.StatusNotImplemented, "CreateUser")
+}
+
+func GetUser(c *gin.Context) {
+
+}
+
+func SearchUser(c *gin.Context) {
+
+}
