2a3d1a8ef4be8217e8e86a0d19c6c092714baecb
Azzam Jihad Ulhaq <azzam.jihad@mekari.com>
2021-11-12 16:36:19 +0700

    use ShouldBindJSON instead

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,13 +1,30 @@
 package users
 
 import (
+	"bookstore_users-api/domain/users"
+	"bookstore_users-api/services"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "CreateUser")
+	var user users.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		fmt.Println(err.Error())
+		// TODO: return bad request to the caller
+		return
+	}
+
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		fmt.Println(saveErr.Error())
+		// TODO: Handle user creation error
+		return
+	}
+
+	c.JSON(http.StatusCreated, result)
 }
 
 func GetUser(c *gin.Context) {
