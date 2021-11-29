5ecd609bb55bd6b7ad055954157ee58dea1ef270
Azzam Jihad Ulhaq <azzam.jiul@gmail.com>
2021-11-29 17:00:46 +0700

    user marshall for public and internal user

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -33,7 +33,7 @@ func Create(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusCreated, result)
+	c.JSON(http.StatusCreated, result.Marshall(c.GetHeader("X-Public") == "True"))
 }
 
 func Get(c *gin.Context) {
@@ -49,7 +49,7 @@ func Get(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK, user)
+	c.JSON(http.StatusOK, user.Marshall(c.GetHeader("X-Public") == "True"))
 }
 
 func Update(c *gin.Context) {
@@ -76,7 +76,7 @@ func Update(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK, result)
+	c.JSON(http.StatusOK, result.Marshall(c.GetHeader("X-Public") == "True"))
 }
 
 func Delete(c *gin.Context) {
@@ -102,5 +102,5 @@ func Search(c *gin.Context) {
 		c.JSON(err.Status, err)
 		return
 	}
-	c.JSON(http.StatusOK, users)
+	c.JSON(http.StatusOK, users.Marshall(c.GetHeader("X-Public") == "True"))
 }
--- services/users_service.go
+++ services/users_service.go
@@ -78,7 +78,7 @@ func DeleteUser(userId int64) *errors.RestErr {
 	return current.Delete()
 }
 
-func Search(status string) ([]users.User, *errors.RestErr) {
+func Search(status string) (users.Users, *errors.RestErr) {
 	dao := &users.User{}
 	return dao.FindByStatus(status)
 }
